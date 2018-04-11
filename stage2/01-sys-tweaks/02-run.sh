6702fa06f6c41159e60ea5d3f95f13c7e043e843
Teppo Kurki <teppo.kurki@iki.fi>
2018-04-11 17:57:08 +0000

    feature: config files & persistence
    Update docker compose config to map host
    directories to the containers so that config
    changes, logs and data are persisted.
    
    Add docker compose files that support
    writing container

--- stage2/01-sys-tweaks/02-run.sh
+++ stage2/01-sys-tweaks/02-run.sh
@@ -10,13 +10,25 @@ apt-get -y install python3-pip
 pip3 install docker-compose
 EOF
 
-on_chroot << EOF
-cd /home/pi && mkdir signalk && cd signalk && curl -fsSL  https://api.github.com/repos/tkurki/signalk-server-docker-setup/tarball | tar xz --strip-components=1
-EOF
+echo `which install`
 
 install -m 644 files/signalk-docker-compose.service ${ROOTFS_DIR}/etc/systemd/system/signalk-docker-compose.service
 mkdir ${ROOTFS_DIR}/etc/signalk-docker-compose
 install -m 644 files/docker-compose.yml ${ROOTFS_DIR}/etc/signalk-docker-compose/docker-compose.yml
+install -m 644 files/docker-compose.env "${ROOTFS_DIR}/etc/signalk-docker-compose/.env"
+
+install -v -d "${ROOTFS_DIR}/home/pi/signalk-docker-data"
+install -v -d -o 999 -g 999 "${ROOTFS_DIR}/home/pi/signalk-docker-data/logs"
+install -v -d "${ROOTFS_DIR}/home/pi/signalk-docker-data/influxdb"
+install -v -d "${ROOTFS_DIR}/home/pi/signalk-docker-data/grafana"
+
+install -v -d "${ROOTFS_DIR}/home/pi/.signalk/plugin-config-data"
+install -m 644 files/dotsignalk/package.json ${ROOTFS_DIR}/home/pi/.signalk/package.json
+install -m 644 files/dotsignalk/security.json ${ROOTFS_DIR}/home/pi/.signalk/security.json
+install -m 644 files/dotsignalk/settings.json ${ROOTFS_DIR}/home/pi/.signalk/settings.json
+chown -R 999:999 ${ROOTFS_DIR}/home/pi/.signalk/
+install -v -d "${ROOTFS_DIR}/home/pi/.signalk/plugin-config-data"
+install -m 644 files/dotsignalk/plugin-config-data/signalk-to-influxdb.json ${ROOTFS_DIR}/home/pi/.signalk/plugin-config-data/signalk-to-influxdb.json
 
 on_chroot << EOF
 systemctl enable signalk-docker-compose
